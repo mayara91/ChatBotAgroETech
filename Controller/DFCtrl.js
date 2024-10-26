1f8efe5e7c8828b24be065357b8a26057634a255
renatofernandosilvagoncalves <renatogoncalves@unoeste.edu.br>
2024-10-26 14:14:27 -0300

    conclusão menu

--- Controller/DFCtrl.js
+++ Controller/DFCtrl.js
@@ -2,7 +2,7 @@ import { obterCardsServicos } from "../DialogFlow/funcoes.js";
 
 export default class DFController {
 
-    processarIntencoes(req, resp) {
+    async processarIntencoes(req, resp) {
         if (req.method == "POST" && req.is("application/json")) {
             const dados = req.body;
             const intencao = dados.queryResult.intent.displayName;
@@ -11,7 +11,7 @@ export default class DFController {
             const origem =  dados?.originalDetectIntentRequest?.source;
             switch (intencao) {
                 case 'Default Welcome Intent':
-                    const resposta = exibirMenu(tipo=origem);
+                    const resposta = await exibirMenu(origem);
                     resp.json(resposta);
                     break;
             }
@@ -19,85 +19,89 @@ export default class DFController {
 
     } //fim processar intenções
 
-    exibirMenu(tipo = 'custom') {
-        let resposta = {
-            "fulfillmentMessages": []
-        };
+}
 
-        try {
-            let cards = obterCardsServicos(tipo);
+async function exibirMenu(tipo = '') {
+    let resposta = {
+        "fulfillmentMessages": []
+    };
+    
+    if (tipo){
+        tipo = 'custom';
+    }
 
-            if (tipo == custom) {
-                resposta['fulfillmentMessages'].push({
-                    "text": {
-                        "text": ["Seja bem-vindo ao suporte de TI.\n",
+    try {
+        let cards = await obterCardsServicos(tipo);
+
+        if (tipo == 'custom') {
+            resposta['fulfillmentMessages'].push({
+                "text": {
+                    "text": ["Seja bem-vindo ao suporte de TI.\n",
+                        "Estamos disponíveis 24h por dia e 7 dias na semana.\n",
+                        "Estamos preparados para te ajudar com os seguintes serviços:\n"
+                    ]
+                }
+            });
+            resposta['fulfillmentMessages'].push(...cards);
+            resposta['fulfillmentMessages'].push({
+                "text": {
+                    "text": ["Por favor nos informe o que você deseja."]
+                }
+            });
+            return resposta;
+        }
+        else {
+            //formato de resposta para o ambiente messenger
+
+            resposta.fulfillmentMessages.push({
+                "payload": {
+                    "richContent": [[{
+                        "type": "description",
+                        "title": "Seja bem-vindo ao suporte de TI.\n",
+                        "text": [
                             "Estamos disponíveis 24h por dia e 7 dias na semana.\n",
                             "Estamos preparados para te ajudar com os seguintes serviços:\n"
                         ]
-                    }
-                });
-                resposta['fulfillmentMessages'].push(...cards);
-                resposta['fulfillmentMessages'].push({
-                    "text": {
-                        "text": ["Por favor nos informe o que você deseja."]
-                    }
-                });
-                return resposta;
-            }
-            else {
-                //formato de resposta para o ambiente messenger
+                    }]]
+                }
+            });
+            //adicionando os cards de serviços
+            resposta['fulfillmentMessages'][0]['payload']['richContent'][0].push(...cards);
 
-                resposta.fulfillmentMessages.push({
-                    "payload": {
-                        "richContent": [[{
-                            "type": "description",
-                            "title": "Seja bem-vindo ao suporte de TI.\n",
-                            "text": [
-                                "Estamos disponíveis 24h por dia e 7 dias na semana.\n",
-                                "Estamos preparados para te ajudar com os seguintes serviços:\n"
-                            ]
-                        }]]
-                    }
-                });
-                //adicionando os cards de serviços
-                resposta['fulfillmentMessages'][0]['payload']['richContent'][0].push(...cards);
-
-                resposta['fulfillmentMessages'][0]['payload']['richContent'][0].push({
-                    "type": "description",
-                    "title": "Por favor nos informe o que você deseja.",
-                    "text": []
-                });
-                return resposta;
-            }
-        }
-        catch(erro){
-            if (tipo == custom){
-                resposta['fulfillmentMessages'].push({
-                    "text": {
-                        "text": ["Não foi possível recuperar a lista de suporte dos serviços disponíveis.",
-                                 "Descupe-nos pelo transtorno!",
-                                 "Entre em contato conosco por telefone (18) 3226-1515."
-                        ]
-                    }
-                });
-            }
-            else{ //formato de mensagem para messenger
-                resposta.fulfillmentMessages.push({
-                    "payload": {
-                        "richContent": [[{
-                            "type": "description",
-                            "title": "Não foi possível recuperar a lista de suporte dos serviços disponíveis.\n",
-                            "text": [
-                                "Descupe-nos pelo transtorno!\n",
-                                "Entre em contato conosco por telefone (18) 3226-1515.\n"
-                            ]
-                        }]]
-                    }
-                });
-            }//fim else
+            resposta['fulfillmentMessages'][0]['payload']['richContent'][0].push({
+                "type": "description",
+                "title": "Por favor nos informe o que você deseja.",
+                "text": []
+            });
             return resposta;
         }
-
+    }
+    catch(erro){
+        if (tipo == 'custom'){
+            resposta['fulfillmentMessages'].push({
+                "text": {
+                    "text": ["Não foi possível recuperar a lista de suporte dos serviços disponíveis.",
+                             "Descupe-nos pelo transtorno!",
+                             "Entre em contato conosco por telefone (18) 3226-1515."
+                    ]
+                }
+            });
+        }
+        else{ //formato de mensagem para messenger
+            resposta.fulfillmentMessages.push({
+                "payload": {
+                    "richContent": [[{
+                        "type": "description",
+                        "title": "Não foi possível recuperar a lista de suporte dos serviços disponíveis.\n",
+                        "text": [
+                            "Descupe-nos pelo transtorno!\n",
+                            "Entre em contato conosco por telefone (18) 3226-1515.\n"
+                        ]
+                    }]]
+                }
+            });
+        }//fim else
+        return resposta;
     }
 
 }
